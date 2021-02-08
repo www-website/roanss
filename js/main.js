fc35127285b6f825d1bcf275feed0ea216563ed8
www-website <75985342+www-website@users.noreply.github.com>
2021-02-08 17:48:31 +0530

    asssasff

---
+++ js/main.js
@@ -0,0 +1,131 @@
+ 
+  let index = 0;
+  const totalWorkItems = $(".work-item").length; 
+
+   $(window).on("load",function(){
+      $(".preloader").addClass("loaded");
+   })
+
+  $(document).ready(function () {
+
+    // nav toggle
+    $(".nav-toggle").click(function(){
+      $(".header .nav").slideToggle();
+    })
+    $(".header .nav a").click(function(){
+      if($(window).width() < 768){
+         $(".header .nav").slideToggle();
+      }
+    })
+
+    // fixed header 
+    $(window).scroll(function(){
+      if($(this).scrollTop() > 100){
+        $(".header").addClass("fixed");
+      }
+      else{
+        $(".header").removeClass("fixed");
+      }
+    })
+
+    // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+    
+  	// set lightbox img max height
+  	const wHeight = $(window).height();
+  	$(".lightbox-img").css("max-height",wHeight+"px");
+
+  	// lightbox 
+  	$(".work-item-inner").click(function(){
+  		index = $(this).parent(".work-item").index();
+  		$(".lightbox").addClass("open");
+  		lightboxSlideShow();
+  	})
+  	$(".lightbox .prev").click(function(){
+       if(index == 0){
+       	 index = totalWorkItems-1;
+       }
+       else{
+       	index--;
+       }
+       lightboxSlideShow();
+  	})
+  	$(".lightbox .next").click(function(){
+  		if(index == totalWorkItems-1){
+  			index = 0
+  		}
+  		else{
+  			index++;
+  		}
+  		lightboxSlideShow();
+  	})
+
+  	// close lightbox 
+  	$(".lightbox-close").click(function(){
+  		$(".lightbox").removeClass("open");
+  	})
+
+  	// close lightbox when clicked outside of img-box 
+     $(".lightbox").click(function(event){
+       if($(event.target).hasClass("lightbox")){
+       	 $(this).removeClass("open");
+       }
+     })
+  })
+
+  function lightboxSlideShow(){
+    const imgSrc = $(".work-item").eq(index).find("img").attr("data-large");
+    const category = $(".work-item").eq(index).find("h4").html();
+    $(".lightbox-img").attr("src",imgSrc);
+    $(".lightbox-category").html(category)
+    $(".lightbox-counter").html(totalWorkItems + "/" + (index+1));
+  }
+
+
+$(document).ready(function(){
+
+  $('.service-item-inner').slice(0,3).show();
+
+  $('.Show-More').on('click',function(){
+    $ ('.service-item-inner:hidden').slice(0,3).slideToggle('slow');
+    if($('.service-item-inner:hidden').length == 0){
+      $(this).fadeOut('slow');
+    }
+
+    return false;
+  });
+});
+
+$(document).ready(function(){
+
+  $('.work-item-inner').slice(0,6).show();
+
+  $('.More-Images').on('click',function(){
+    $ ('.work-item-inner:hidden').slice(0,3).slideToggle('slow');
+    if($('.work-item-inner:hidden').length == 0){
+      $(this).fadeOut('slow');
+    }
+
+    return false;
+  });
+});
